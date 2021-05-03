0653061e292abeca8870e10712b56b25f6bc692e
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-05-03 23:55:10 +0100

    initialize cluster module

---
+++ modules/variables.tf
@@ -0,0 +1,23 @@
+ variable "location" {
+    type = string
+    description = "resource location"
+    default = "East US"
+}
+
+variable "kubernetes_version" {
+  type = string
+  description = "k8's version"
+  default = "1.19.6" 
+}
+
+
+variable "admin_user"{
+  type = string
+  description = "username for linux_profile"
+  default = "enderdragon"
+}
+
+
+variable "ssh_key" {
+   description = "ssh_key for admin_user"
+}
