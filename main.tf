52d14a234d7c2dc6a25ba2ae2dccf76c8b538503
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-05-03 23:54:22 +0100

    adds terraform entry point

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.57.0"
+    }
+  }
+}
+
+provider "azurerm" {
+   features{}
+}
+
+module "cluster" {
+  source                = "./modules/cluster"
+  ssh_key               = var.ssh_key
+  location              = var.location
+  kubernetes_version    = var.kubernetes_version  
+  
+}
+
